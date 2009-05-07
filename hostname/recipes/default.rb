75e03e2404b55a55a1b922f42ad6d40d0ddb60a7
Michael Hale <mikehale@gmail.com>
2009-05-07 15:37:26 -0400

    hostname cookbook for updating /etc/hostname and /etc/hosts from an attribute

---
+++ hostname/recipes/default.rb
@@ -0,0 +1,22 @@
+if node[:fully_qualified_domain_name]
+  node[:fqdn] = node[:fully_qualified_domain_name]
+  node[:hostname], node[:domain] = node[:fqdn].split('.', 2)
+
+  template "/etc/hosts" do
+    source "hosts.erb"
+    mode 0644
+    owner "root"
+    group "root"
+  end
+
+  template "/etc/hostname" do
+    source "hostname.erb"
+    mode 0644
+    owner "root"
+    group "root"
+  end
+
+  execute "set hostname" do
+    command "/bin/hostname --file /etc/hostname"
+  end
+end
