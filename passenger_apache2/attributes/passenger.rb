a1c89bfc0e217ecc6481d9a4bf4f7d4bd7499d00
Michael Hale <mikehale@gmail.com>
2009-09-10 23:10:10 -0400

    whoops

--- passenger_apache2/attributes/passenger.rb
+++ passenger_apache2/attributes/passenger.rb
@@ -3,4 +3,4 @@ set[:passenger][:root_path]      = "#{languages[:ruby][:gems_dir]}/gems/passenge
 set[:passenger][:module_path]    = "#{passenger[:root_path]}/ext/apache2/mod_passenger.so"
 set_unless[:passenger][:config]  = {}
 
-set_unless[:languages][:ruby][:gem_bin] = node[:languages][:ruby][:ruby_bin].gsub("ruby", "gem")
+set_unless[:languages][:ruby][:gem_bin] = languages[:ruby][:ruby_bin].gsub("ruby", "gem")
